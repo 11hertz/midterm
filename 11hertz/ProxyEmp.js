671171d5149d92a3989b95d6901c08b06023ffe7
11hertz <reachzin@naver.com>
2024-07-06 16:31:26 +0900

    cal, ProxyEmp, weeks by 11hertz

--- 11hertz/ProxyEmp.js
+++ 11hertz/ProxyEmp.js
@@ -5,11 +5,25 @@ export class ProxyEmp {
   constructor() {
     return new Proxy(this, {
       get(target, prop) {
-        // 이 부분을 작성하시오.
+        if (prop === 'fullName') {
+          return `${target.firstName} ${target.lastName}`;
+        } else {
+          return target[prop];
+        }
       },
 
       set(target, prop, value) {
         // 이 부분을 작성하시오.
+        const [firstName, lastName] = value.split(' ');
+        if (prop === 'fullName') {
+          if (lastName !== undefined) {
+            target.firstName = firstName;
+            target.lastName = lastName?.toUpperCase();
+          } else {
+            target.lastName = firstName;
+          }
+        }
+        return target;
       },
     });
   }
--- 11hertz/cal.js
+++ 11hertz/cal.js
@@ -4,8 +4,30 @@ export function cal(ym) {
   let ret = '';
 
   // 이 부분을 완성하시오.
+  const yearMonth = ym.split('-');
+  const y = yearMonth[0];
+  const m = +yearMonth[1];
+
+  let startDay = new Date(y, m - 1, 1).getDay();
+  const endDate = new Date(y, m, 0).getDate();
+
+  let count = 0;
+  while (count <= endDate) {
+    let line = '';
+    for (let j = 0; j < 7; j++) {
+      if (count === endDate) break;
+      if (j < startDay) line += '   ';
+      else {
+        count += 1;
+        line += count.toString().padStart(2, ' ') + ' ';
+      }
+    }
+    ret += line + '\n';
+    if (count === endDate) break;
+    startDay = 0;
+  }
 
   console.log(`${m}월 ${y}`.padStart(21 / 2 + 4, ' '));
-  console.log([...WEEKS].map(w => w.padStart(2, ' ')).join(''));
+  console.log([...WEEKS].map((w) => w.padStart(2, ' ')).join(''));
   console.log(ret);
 }
